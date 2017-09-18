1b8c789229011e3f5de0048e4bd46b826b36d736
ed85w <ed.walker@hotmail.co.uk>
2017-09-18 20:47:29 +0100

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.db import models
+from django.utils import timezone
+
+
+class AccountUserManager(UserManager):
+    def _create_user(self, username, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+       Creates and saves a User with the given username, email and password.
+       """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given username must be set')
+ 
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+ 
+        return user
+ 
+class User(AbstractUser):
+
+	wedding_role = models.ForeignKey(WeddingRole, related_name='users')
+	stripe_id = models.CharField(max_length=40, default='')
+ 
+    objects = AccountUserManager()
+
+
+class WeddingRole(models.Model):
+
+    wedding_role = models.CharField()
