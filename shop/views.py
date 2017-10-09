fd6f671c07ecfbda4f2bcb21c850c6ad96b5c945
ed85w <ed.walker@hotmail.co.uk>
2017-10-09 21:03:25 +0100

    add to cart is starting to take shape, not quite working for updating when a product already exists in cart/session

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,64 @@
 from django.shortcuts import render, get_object_or_404
 from models import Product, ProductReview, ProductImage, ProductCategory
-from .forms import ProductReviewForm
+from .forms import ProductReviewForm, AddToCartForm
 from django.shortcuts import redirect
 from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from django.template.context_processors import csrf
-
+from decimal import Decimal
 
 def shop(request):
     return render(request, 'shop/shop.html', {'products': Product.objects.all()})
 
 
 def product_detail(request, product_id):
+    request.session.set_expiry(120000)
     product = get_object_or_404(Product, pk=product_id)
-    return render(request, 'shop/product_detail.html', {'product': product})
+
+    if request.method == "POST":
+        form = AddToCartForm(request.POST)
+        if form.is_valid():
+
+            # if cart already exists
+            if request.session['cart']:
+                print "cart already exists"
+                if product_id in request.session['cart']:
+                    print "product already in cart"
+                    to_add = {product.id: {
+                        'product_title': product.title,
+                        'product_price': "%.2f" % Decimal(product.price),
+                        'quantity': form.cleaned_data['quantity'],
+                        'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
+                    print to_add
+                    request.session['cart'].update(to_add)
+                    print request.session['cart']
+                else:
+                    to_add = {product.id: {
+                        'product_title': product.title,
+                        'product_price': "%.2f" % Decimal(product.price),
+                        'quantity': form.cleaned_data['quantity'],
+                        'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
+                    print to_add
+                    request.session['cart'].update(to_add)
+                    print request.session['cart']
+
+            # if cart doesn't already exist
+            else:
+                request.session['cart'] = {}
+                cart = request.session['cart']
+                to_add = {product.id: {
+                    'product_title': product.title,
+                    'product_price': "%.2f" % Decimal(product.price),
+                    'quantity': form.cleaned_data['quantity'],
+                    'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
+                cart.update(to_add)
+        # print request.session['cart']
+        # return redirect(reverse('product_detail', args={product.pk}))
+
+    else:
+        form = AddToCartForm({'product': product, 'quantity': 1})
+
+    return render(request, 'shop/product_detail.html', {'product': product, 'form': form})
 
 
 def add_review(request, product_id):
@@ -46,8 +91,3 @@ def add_review(request, product_id):
     args.update(csrf(request))
 
     return render(request, 'shop/add_review.html', args)
-
-
-
-
-
