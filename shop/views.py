0a3609e95b27eca8d8ee4ec070d78b45d4ab5471
ed85w <ed.walker@hotmail.co.uk>
2017-10-10 20:37:47 +0100

    session rendering in cart view, not yet able to target specific fields

--- shop/views.py
+++ shop/views.py
@@ -7,6 +7,9 @@ from django.core.urlresolvers import reverse
 from django.template.context_processors import csrf
 from decimal import Decimal
 
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 def shop(request):
     return render(request, 'shop/shop.html', {'products': Product.objects.all()})
 
@@ -20,39 +23,28 @@ def product_detail(request, product_id):
         if form.is_valid():
 
             # if cart already exists
-            if request.session['cart']:
-                print "cart already exists"
-                if product_id in request.session['cart']:
-                    print "product already in cart"
-                    to_add = {product.id: {
-                        'product_title': product.title,
-                        'product_price': "%.2f" % Decimal(product.price),
-                        'quantity': form.cleaned_data['quantity'],
-                        'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
-                    print to_add
-                    request.session['cart'].update(to_add)
-                    print request.session['cart']
-                else:
-                    to_add = {product.id: {
-                        'product_title': product.title,
-                        'product_price': "%.2f" % Decimal(product.price),
-                        'quantity': form.cleaned_data['quantity'],
-                        'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
-                    print to_add
-                    request.session['cart'].update(to_add)
-                    print request.session['cart']
-
-            # if cart doesn't already exist
-            else:
-                request.session['cart'] = {}
-                cart = request.session['cart']
-                to_add = {product.id: {
+            try:
+                request.session['cart']
+                to_add = {product_id: {
                     'product_title': product.title,
                     'product_price': "%.2f" % Decimal(product.price),
                     'quantity': form.cleaned_data['quantity'],
                     'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
-                cart.update(to_add)
-        # print request.session['cart']
+                request.session['cart'].update(to_add)
+                messages.success(request, 'Product Added to Cart!')
+
+            # if cart doesn't already exist
+            except KeyError:
+                print "cart does not exist"
+                request.session['cart'] = {}
+                to_add = {product_id: {
+                    'product_title': product.title,
+                    'product_price': "%.2f" % Decimal(product.price),
+                    'quantity': form.cleaned_data['quantity'],
+                    'subtotal': "%.2f" % (product.price * form.cleaned_data['quantity'])}}
+                request.session['cart'].update(to_add)
+                messages.success(request, 'Product Added to Cart!')
+        print request.session['cart']
         # return redirect(reverse('product_detail', args={product.pk}))
 
     else:
@@ -91,3 +83,13 @@ def add_review(request, product_id):
     args.update(csrf(request))
 
     return render(request, 'shop/add_review.html', args)
+
+
+def cart(request):
+    cart = request.session['cart']
+    print cart
+    for item in cart:
+        print item
+    return render(request, 'shop/cart.html', {'cart': cart})
+
+
