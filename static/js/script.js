ba512c2e3a07965e54440db6c069bfb974758ff5
ed85w <ed.walker@hotmail.co.uk>
2017-10-16 19:59:26 +0100

    basket updates on change to products, working on extracting table data as JSON

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,40 @@
 /**
  * Created by Ed on 25/09/2017.
  */
-$(document).ready(function() {
+$(document).ready(function(){
 
+    // function to update basket subtotals and total when number of items changes
+    $('table tr td input').on('input', function() {
+        var id = $(this).attr('id').substring(16,17);
+        var price = parseFloat($('#' + 'basket-itm-price' + id).text().substring(1,7));
+        var quantity = $('#' + 'basket-qty-input' + id).val();
+        var currSubtotal = parseFloat($('#' + 'basket-itm-subtotal' + id).text().substring(1,7));
+        var newSubtotal = parseFloat(price * quantity).toFixed(2);
+        var difference = parseFloat(newSubtotal - currSubtotal);
+        var currTotal = parseFloat($('#basket-total').text().substring(1,7));
+        var newTotal = parseFloat(currTotal + difference).toFixed(2);
+        $('#' + 'basket-itm-subtotal' + id).html('£'+ newSubtotal);
+        $('#basket-total').html('£' + newTotal);
+        console.log("current total is" + currTotal);
+        console.log("diff is" + difference);
+        console.log("new total is" + newTotal);
+    });
 
-    $(".nav-item").click(function(){
+    //function for remove from basket button
+    $('table tr td button').on('click', function(){
+        var id = $(this).attr('id').substring(3,4);
+        var row = $('#row'+id);
+        var currSubtotal = parseFloat($('#' + 'basket-itm-subtotal' + id).text().substring(1,7));
+        var currTotal = parseFloat($('#basket-total').text().substring(1,7));
+        var newTotal = parseFloat(currTotal - currSubtotal).toFixed(2);
+        row.remove();
+        $('#basket-total').html('£' + newTotal);
+    });
 
-        var pageId = $(this).attr("id").substring(0,9); //gets class name of element clicked
-        alert(pageId + "hello");
-        $('#home-back').removeClass();
-        $('#home-back').addClass(pageId);
+    $('#checkout-btn').on('click', function() {
+
 
     });
+
 
 });
