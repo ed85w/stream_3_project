576a5ccc5f50fd99fbeb3cfeaeeebd719cbdcdc1
ed85w <ed.walker@hotmail.co.uk>
2017-10-22 14:10:36 +0100

    changes to models and db

--- shop/models.py
+++ shop/models.py
@@ -6,9 +6,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class ProductCategory(models.Model):
     title = models.CharField(max_length=150, default='')
     description = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-    active = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.title
@@ -19,24 +16,18 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     category = models.ManyToManyField(ProductCategory, blank=True)
     main_image = models.ImageField(upload_to='media/')
-    price = models.DecimalField(decimal_places=2, max_digits=10, default='')
+    price = models.DecimalField(max_digits=11, decimal_places=2,  null=True, blank=True)
     stock = models.IntegerField()
-    slug = models.SlugField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.title
 
-    def get_price(self):
-        return self.price
-
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product)
     image = models.ImageField(upload_to='media/')
-    featured = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
