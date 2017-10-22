c93e97f0ebfdc61eebc5235429fd07c7eca82e96
ed85w <ed.walker@hotmail.co.uk>
2017-10-22 12:22:24 +0100

    heroku app added to allowed hosts

--- settings/staging.py
+++ settings/staging.py
@@ -16,8 +16,8 @@ DATABASES = {
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_WPgPzAQeCfea08WkskzTqxii')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_6vCTHyEBILOhXzg3gfMd9U8w')
 
-SITE_URL = 'https://your-heroku-app.herokuapp.com'
-ALLOWED_HOSTS.append('your-heroku-app.herokuapp.com')
+SITE_URL = 'https://wedding-stationery-shop.herokuapp.com/'
+ALLOWED_HOSTS.append('wedding-stationery-shop.herokuapp.com')
 
 # Log DEBUG information to the console
 LOGGING = {
