a38a1c504d2c544998997b84db2a6edb6a191837
ed85w <ed.walker@hotmail.co.uk>
2017-10-22 12:31:39 +0100

    database settings added

--- settings/base.py
+++ settings/base.py
@@ -90,7 +90,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
--- settings/staging.py
+++ settings/staging.py
@@ -1,15 +1,20 @@
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
 # Stripe environment variables
