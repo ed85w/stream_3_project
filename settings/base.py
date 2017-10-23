429813ba1d7930506ee9e9bec642553cf166eb90
ed85w <ed.walker@hotmail.co.uk>
2017-10-23 18:17:47 +0100

    test - using same settings for static files as media. media works online

--- settings/base.py
+++ settings/base.py
@@ -90,11 +90,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+# commented out to test matching with media setup
+# STATIC_URL = '/static/'
+# STATIC_ROOT = ''
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
